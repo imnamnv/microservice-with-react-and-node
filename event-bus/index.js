c43fcb5aab450d7b92b1f2a304259b0d313cb983
NamNV <imnamnv@gmail.com>
2023-10-07 16:37:18 +0700

    added event data store, moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((error) => {
     console.log("error", error.message);
@@ -17,8 +21,16 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((error) => {
     console.log("error", error.message);
   });
+  axios.post("http://localhost:4003/events", event).catch((error) => {
+    console.log("error", error.message);
+  });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  console.log("events", events);
+  res.send(events);
 });
 
 app.listen(4005, () => {
